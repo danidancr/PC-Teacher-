fdfc2ab0aa5d6f63d4aec2130ff32c9693a1065c
danidancr <129102611+danidancr@users.noreply.github.com>
2025-10-26 02:06:54 -0400

    commit 8
    Removed models in favor of Firestore structure and added new routes for course information.

--- app.py
+++ app.py
@@ -55,15 +55,6 @@ elif not firebase_admin._apps:
     print("ERRO CRÍTICO: Firebase Admin SDK não foi inicializado. Verifique as credenciais.")
 
 # =========================================================
-# 2. MODELOS (REMOVIDO e SUBSTITUÍDO por Estrutura Firestore)
-# =========================================================
-# As classes Usuario, Progresso e RespostaProjeto foram removidas.
-# No Firestore, os dados serão armazenados em Coleções (Collections).
-# Coleções: 'usuarios', 'progresso', 'respostas_projeto'
-# ---------------------------------------------------------
-
-
-# =========================================================
 # 3. HELPERS E DECORATORS (REVISADOS)
 # =========================================================
 
@@ -330,6 +321,31 @@ def logout():
     session.pop('usuario_id', None)
     flash('Você saiu da sua conta.', 'info')
     return redirect(url_for('index'))
+
+# =========================================================
+# 4.1 INFORMAÇÃO
+# =========================================================
+
+@app.route('/infor-curso-decomposicao')
+def infor_curso_decomposicao():
+    usuario = usuario_logado()
+    return render_template('infor-curso-decomposicao.html', user=usuario)
+
+@app.route('/infor-curso-rec-padrao')
+def infor_curso_rec_padrao():
+    usuario = usuario_logado()
+    return render_template('infor-curso-rec-padrao.html', user=usuario)
+
+@app.route('/infor-curso-abstracao')
+def infor_curso_abstracao():
+    usuario = usuario_logado()
+    return render_template('infor-curso-abstracao.html', user=usuario)
+
+@app.route('/infor-curso-algoritmo')
+def infor_curso_algoritmo():
+    usuario = usuario_logado()
+    return render_template('infor-curso-algoritmo.html', user=usuario)
+
 
 
 # =========================================================
